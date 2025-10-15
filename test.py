b0f0d1d9bccff1b8b0535d38bc1c9b0ed985ff71
Swetha <sevuganswetha@gmail.com>
2025-10-15 20:10:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 from app import helloworld
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(helloworld, "Hello_World")
+        self.assertEqual(helloworld(), "Hello_World")
 
 if __name__=="_main_":
     unittest.main()
